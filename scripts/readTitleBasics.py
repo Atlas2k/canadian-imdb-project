0dd1fb85a22a13681a37736424f8a144abc79729
Atlas2k <amouri2000@hotmail.com>
2022-11-10 16:21:17 -0600

    Update readTitleBasics.py to Handle Genre Data

--- scripts/readTitleBasics.py
+++ scripts/readTitleBasics.py
@@ -14,15 +14,18 @@ import sys
 csv.field_size_limit(sys.maxsize)
 """titleId, primaryTitle, isAdult, startYear, endYear, runTimeMinutes"""
 titlesData = [[0, "", 0, 0, 0, 0]]
+genres = {}
+titleGenres = [] # part of 
 
 with open("title.basics.tsv") as file:
     titles = csv.reader(file, delimiter="\t")
     i = 0
+    genreId = 1
     for line in titles:
         temp = []
         if i != 0:
-            if line[1] == "movie" or line[1] == "tvSeries" or line[1] == "tvEpisode":
-                temp.append(int(line[0][2:])) # titleID
+            if (line[1] == "movie" or line[1] == "tvSeries" or line[1] == "tvEpisode") and len(line) == 9:
+                temp.append(int(line[0][2:]))  # titleID
                 temp.append(line[3])  # title
                 temp.append(line[4])  # isAdult
                 temp.append(line[5])  # startYear
@@ -32,10 +35,35 @@ with open("title.basics.tsv") as file:
                     if temp[i] == "\\N":
                         temp[i] = "null"
                 titlesData.append(temp)
+                # Take care of genres
+                tempGenres = line[8].split(",")
+                for genre in tempGenres:
+                    if genre == "\\N":
+                        testGenre = "null"
+                    else:
+                        testGenre = genre
+                    if testGenre not in genres.keys():
+                        genres[testGenre] = genreId
+                        genreId += 1
+                for genre in tempGenres:
+                    if genre == "\\N":
+                        titleGenres.append([temp[0], genres["null"]])
+                    else:
+                        titleGenres.append([temp[0], genres[genre]])
         i += 1
 
 sqlFile = open("title.basics.sql", "w")
 for value in titlesData:
-    preparedSql = "update table media set title=\"%s\", isAdult=%s, startYear=%s, endYear=%s, runtime=%s where titleid = %s\n;" % (
+    preparedSql = "update table media set title=\"%s\", isAdult=%s, startYear=%s, endYear=%s, runtime=%s where titleid = %s;\n" % (
         value[1], value[2], value[3], value[4], value[5], value[0])
     sqlFile.write(preparedSql)
+
+sqlFileGenres = open("genres.title.basics.sql", "w")
+for value in genres:
+    preparedSql = "insert into table genre (genreid, genreName) values (%s, \"%s\");\n" % (genres[value], value)
+    sqlFileGenres.write(preparedSql)
+
+sqlFilePartOf = open("partOf.title.basics.sql", "w")
+for value in titleGenres:
+    preparedSql = "inset into table partOf (genreid, titleid) value (%s, %s);\n" % (value[1], value[0])
+    sqlFilePartOf.write(preparedSql)
