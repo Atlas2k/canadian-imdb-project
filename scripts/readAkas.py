a24dc706cddd907bbf8a8f64802b0fb79135863d
Atlas2k <amouri2000@hotmail.com>
2022-11-13 11:33:57 -0600

    Updated readAkas.py Script

--- scripts/readAkas.py
+++ scripts/readAkas.py
@@ -9,18 +9,20 @@ import sys
     [5] = types
     [6] = attributes
     [7] = isOriginalTitle
+    [8] = Genre
 """
 csv.field_size_limit(sys.maxsize)
-keptTitlesData = [[0, "", ""]]
+keptTitlesData = [[0, "null", ""]]
+
 
 with open("title.akas.tsv") as file:
     titles = csv.reader(file, delimiter="\t")
     i = 0
     for line in titles:
         temp = []
-        if i != 0 and line[3] == "US" and (int(line[0][2:]) != keptTitlesData[len(keptTitlesData) - 1][0]):
+        if i != 0 and (line[3] == "US" or line[3] == "CA") and (int(line[0][2:]) != keptTitlesData[len(keptTitlesData) - 1][0]):
             temp.append(int(line[0][2:]))
-            temp.append(line[2])
+            temp.append("null")
             if line[4] == "\\N":
                 temp.append("null")
             else:
@@ -30,5 +32,6 @@ with open("title.akas.tsv") as file:
 
 sqlFile = open("title.akas.sql", "w")
 for value in keptTitlesData:
-    preparedSql = "insert into table media (titleid, title, language) values (%s, %s, %s)\n" % (value[0], value[1], value[2])
+    preparedSql = "insert into table media (titleid, title, language) values (%s, \"%s\", \"%s\");\n" % (
+        value[0], value[1], value[2])
     sqlFile.write(preparedSql)
