3d8929d2c71801661b36b17b8e60df3fab1d540b
Mayokun Moses Akintunde <77511590+akintundem@users.noreply.github.com>
2022-11-16 15:34:53 -0600

    Update generateSQL.java

--- src/generateSQL.java
+++ src/generateSQL.java
@@ -16,7 +16,7 @@ public class generateSQL {
 
     public generateSQL() {
         Properties prop = new Properties();
-        String fileName = "auth.cfg";
+        String fileName = "C:\\Users\\akint\\OneDrive\\Desktop\\COMP3380Proj\\Server\\src\\auth.cfg";
         try {
             FileInputStream configFile = new FileInputStream(fileName);
             prop.load(configFile);
@@ -31,92 +31,113 @@ public class generateSQL {
         String username = (prop.getProperty("username"));
         String password = (prop.getProperty("password"));
 
-        if (username == null || password == null) {
+        if (username == null || password == null){
             System.out.println("Username or password not provided.");
             System.exit(1);
         }
 
-        String connectionUrl = "jdbc:sqlserver://uranium.cs.umanitoba.ca:1433;"
-                + "database=cs3380;"
-                + "user=" + username + ";"
-                + "password=" + password + ";"
-                + "encrypt=false;"
-                + "trustServerCertificate=false;"
-                + "loginTimeout=30;";
+        String connectionUrl =
+                "jdbc:sqlserver://uranium.cs.umanitoba.ca:1433;"
+                        + "database=cs3380;"
+                        + "user=" + username + ";"
+                        + "password="+ password +";"
+                        + "encrypt=false;"
+                        + "trustServerCertificate=false;"
+                        + "loginTimeout=30;";
 
         this.resultSet = null;
 
-        try {
-            this.connection = DriverManager.getConnection(connectionUrl);
+        try {this.connection = DriverManager.getConnection(connectionUrl);
 
-            this.statement = connection.createStatement();
-            Tables.dropTables(statement);
-            Tables.createTables(statement);
+             this.statement = connection.createStatement();
+            System.out.println("Exporting ....");
+            // Tables.dropTables(statement);
+            // Tables.createTables(statement);
             loadTables();
 
-        } catch (SQLException | IOException e) {
-            e.printStackTrace();
+        }
+        catch (SQLException | IOException e) {
+             e.printStackTrace();
         }
 
+    }
+
+    public String yourSearch(String input) throws SQLException {
+        queries query = new queries();
+        Parameters p = new Parameters();
+        handleErrors(input.split(" ")[1],p);
+        if(input.split(" ")[0].compareTo("a")==0){
+            return query.searchID(resultSet,statement,p.getActorName());
+        } // a is to seaarch for information about an author.
+
+        return null;
+    }
+
+    public void handleErrors(String parameters, Parameters p){
+        String[] para = parameters.split(",");
+        for(int i =0; i<para.length; i++){
+            if(para[i].split(":")[0].compareTo("Search")==0){
+                p.setSearch(para[i].split(":")[1]);
+            }
+
+            if(para[i].split(":")[0].compareTo("AuthorName")==0){
+                p.setActorName((para[i].split(":")[1]));
+            }
+
+            if(para[i].split(":")[0].compareTo("Shows")==0){
+                p.setActorName((para[i].split(":")[1]));
+            }
+            if(para[i].split(":")[0].compareTo("Episodes")==0){
+                p.setActorName((para[i].split(":")[1]));
+            }
+        }
     }
 
     public void loadTables() throws IOException, SQLException {
         System.out.println("Loading Data From Scripts Into Server. Please Wait...");
         System.out.println("Loading the media table...");
-        loadData("title.akas.sql");
-        loadData("title.basics.sql");
-        loadData("title.ratings.sql");
-        connection.createStatement().execute("Delete from media where titleId = 0;");
-        connection.createStatement().execute("Delete from media where titleId is NULL;");
+        // loadData("title.akas.sql");
+        // loadData("title.basics.sql");
+        // loadData("title.ratings.sql");
+        // connection.createStatement().execute("Delete from media where titleId = 0;");
+        // connection.createStatement().execute("Delete from media where title is NULL;");
 
         System.out.println("Loading the genres table and realtion...");
-        loadData("genres.title.basics.sql");
-        loadData("partOf.title.basics.sql");
+        // loadData("genres.title.basics.sql");
+        // loadData("partOf.title.basics.sql");
 
         System.out.println("Loading the having (episodes) table...");
-        loadData("title.episode.sql");
+        // loadData("title.episode.sql");
 
         // Loading the people table
         System.out.println("Loading the people table relations...");
-        loadData("name.basics.sql");
-        loadData("knownFor.name.basics.sql");
-        loadData("jobs.names.basics.sql");
-        loadData("works.name.basics.sql");
-        connection.createStatement().execute("Delete from people where personId = 0;");
+        // loadData("name.basics.sql");
+        // loadData("knownFor.name.basics.sql");
+        // loadData("jobs.name.basics.sql");
+        // loadData("works.name.basics.sql");
 
         // Loading cast and character data
         System.out.println("Loading the cast and character tables...");
-        loadData("title.principals.sql");
-        loadData("characters.title.principals.sql");
+        //        loadData("title.principals.sql");
+        //        loadData("characters.title.principals.sql");
 
         // Loading platform data
         System.out.println("Loading the platform table and relations...");
-        loadData("platform.sql");
-        loadData("availableOn.sql");
+        //         loadData("platform.sql");
+        //         loadData("availableOn.sql");
     }
 
-    public String yourSearch(String input) throws SQLException {
-        queries query = new queries();
-        if (input.split(" ")[0].compareTo("a") == 0) {
-            return query.searchID(resultSet, statement, input.split(" ")[1]);
-        } // a is for searching by id
 
-        if (input.split(" ")[0].compareTo("b") == 0) {
-            return query.searchName(resultSet, statement, input.split(" ")[1]);
-        } // a is for searching by id
-
-        return null;
-    }
 
     public void loadData(String script) throws IOException, SQLException {
         BufferedReader reader = new BufferedReader(new FileReader(script));
         String line = reader.readLine();
         // assumes each query is its own line
+        System.out.println(line);
         while (line != null) {
             this.connection.createStatement().execute(line);
             line = reader.readLine();
         }
         reader.close();
     }
-
 }
